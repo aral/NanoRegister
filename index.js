2ee16ddcc1116873aa14f40200509488a01b306a
Lior Subotnick <theawesomepandapig@gmail.com>
2021-03-10 00:38:35 -0800

    Added link to repo

--- index.js
+++ index.js
@@ -454,3 +454,16 @@ function cancelBack() {
 function hasUpperCase(str) {
     return str.toLowerCase() != str;
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById("repo-link").style.borderColor = color;
+}
+
+function resetColor() {
+    document.getElementById("repo-link").style.borderColor = "white";
+}
